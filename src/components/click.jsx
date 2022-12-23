9d11679a126891928b3b16f7ab2b6685ba374040
01star01ek <01star01ek@gmail.com>
2022-12-23 17:19:39 +0900

    select done

---
+++ src/components/click.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const Click = ({text, color}) => {
+    return(
+        <div css = {css `
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        `} >
+        <div css = { css` display: flex;
+                flex-direction: vertex;
+                align-items: center;
+               `}
+                ><p css = {css
+                    `font-family : big;
+                    font-size : 1.125rem;
+                    color : ${color} `
+                }>{text}</p>
+            
+        </div>
+        <button css ={
+            css`
+            border-radius: 100%;
+            border : 3px solid #907D5A;
+            width : 10vmin;
+            height : 10vmin;
+            background-color : white;
+            margin-top : -12px
+
+            `
+        }>
+        </button>
+        </div>
+    );
+}
+
+export default Click;
